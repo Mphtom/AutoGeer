ecfc9a0b135228f7813f5d2203b7d30642308fae
fatma-mohamed99 <fatma.99.mph@gmail.com>
2024-09-01 10:41:48 +0300

    Added favorite logic and style updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,19 +6,15 @@ import { CardComponent } from './card/card.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { NotfondcomponentComponent } from './notfondcomponent/notfondcomponent.component';
 import { IndexPageComponent } from './index-page/index-page.component';
-<<<<<<< HEAD
 import { ProductsCategoryComponent } from './products-category/products-category.component';
 import { FavoritesComponent } from './favorites/favorites.component';
-=======
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AuthGuard } from '../../auth.guard';
-
-
 import { AdminComponent } from './admin/admin.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { AllProductsComponent } from './all-products/all-products.component';
 import { EditComponent } from './edit/edit.component';
->>>>>>> 3ffbc7ac9856f6e107377b441fabb15686c27ea0
+
 export const routes: Routes = [
     {
         path: '',
@@ -27,13 +23,7 @@ export const routes: Routes = [
     {
         path: 'products',
         component: MainPageComponent
-<<<<<<< HEAD
-    }
-
-    ,
-=======
     },
->>>>>>> 3ffbc7ac9856f6e107377b441fabb15686c27ea0
     {
         path: 'login',
         component: LoginComponent
@@ -46,50 +36,36 @@ export const routes: Routes = [
         path: 'registration',
         component: RegistrationComponent
     },
-    { path: 'editProduct/:id', component: EditComponent },
-
-    
+    {
+        path: 'editProduct/:id',
+        component: EditComponent
+    },
     {
         path: 'card',
         component: CardComponent
     },
-    // start: nested routes
     {
         path: 'admin',
         component: AdminComponent,
-        canActivate: [AuthGuard], // حماية مسار الإدارة والفرعيات
+        canActivate: [AuthGuard],
         children: [
             { path: 'allproducts', component: AllProductsComponent },
             { path: 'addProduct', component: AddProductComponent }
-            // تعديل المنتج
-
-
         ]
     },
-    // end: nested routes
     {
         path: 'product-details/:id',
         component: ProductDetailsComponent
     },
-<<<<<<< HEAD
-
-
-
-
     {
         path: 'category-products/:category',
         component: ProductsCategoryComponent
     },
-
     {
         path: 'favorites',
         component: FavoritesComponent
     },
-
     {
-=======
-    {
->>>>>>> 3ffbc7ac9856f6e107377b441fabb15686c27ea0
         path: '**',
         component: NotfondcomponentComponent
     }
